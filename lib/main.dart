5f6916627491ac5e1b68e2631ea89767b11ce387
OnlyFeatures <anya1470@yandex.ru>
2021-12-01 21:01:49 +0300

    added search page, user page, Book class

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyHomePage extends StatefulWidget {
 class MyHomePageState extends State<MyHomePage> {
   int selectedIndex = 0;
   Widget _homePage = HomePage();
-  Widget _searchPage = SearchPage();
+  Widget _searchPage = SearchList();
   Widget _shelfPage = ShelfPage();
   Widget _myProfile = MyProfile();
 
@@ -150,6 +150,7 @@ class User extends StatefulWidget{
   int id = 0;
   Image im = Image.asset("", fit: BoxFit.contain);
 
+
   User({ Key? key, required this.name, required this.id, required this.im}) : super(key: key);
   @override
   _UserState createState() => _UserState(this.name, this.id, this.im);
@@ -210,7 +211,7 @@ class _UserState extends State<User>{
             Row(children: <Widget>[
               Expanded(
                   child: InkWell(
-                    onTap: () {Navigator.push(context, MaterialPageRoute(builder: (context) => OrderPage()));},
+                    onTap: () {Navigator.push(context, MaterialPageRoute(builder: (context) => SearchList()));},
                     child: Container(child: Text('Заказать доставку', style: TextStyle(fontSize: 25), textAlign: TextAlign.left),
                       padding: EdgeInsets.fromLTRB(20, 10, 0, 10),
                       margin: EdgeInsets.fromLTRB(0, 40, 0, 0),
@@ -259,7 +260,174 @@ class OrderPage extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(title: Text('Заказать'),
             backgroundColor: Colors.amber),
-        body: Center(child: Text('Сделать страницу заказа', style: TextStyle(fontSize: 25)))
+        body: Center(child: Text('Вообще-то тут должна быть страница поиска книги', style: TextStyle(fontSize: 25)))
     );
   }
+}
+
+class Book extends StatefulWidget{
+
+  String name = "";
+  int id = 0;
+  Image im = Image.asset("", fit: BoxFit.contain);
+
+  Book({ Key? key, required this.name, required this.id, required this.im}) : super(key: key);
+  @override
+  _BookState createState() => _BookState(this.name, this.id, this.im);
+}
+
+class _BookState extends State<Book> {
+
+  String name = "";
+  int id = 0;
+  Image im = Image.asset("", fit: BoxFit.contain);
+
+  _BookState(this.name, this.id, this.im);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('book');
+  }
+
+  }
+
+class SearchList extends StatefulWidget {
+  SearchList({ Key? key }) : super(key: key);
+  @override
+  _SearchListState createState() => new _SearchListState();
+
+}
+
+class _SearchListState extends State<SearchList>
+{
+  Widget appBarTitle = new Text("Поиск", style: new TextStyle(color: Colors.white),);
+  Icon actionIcon = new Icon(Icons.search, color: Colors.white,);
+  final key = new GlobalKey<ScaffoldState>();
+  final TextEditingController _searchQuery = new TextEditingController();
+  List<String> _list = [];
+  bool _IsSearching = true;
+  String _searchText = "";
+
+  _SearchListState() {
+    _searchQuery.addListener(() {
+      if (_searchQuery.text.isEmpty) {
+        setState(() {
+          _IsSearching = false;
+          String _searchText = "";
+        });
+      }
+      else {
+        setState(() {
+          _IsSearching = true;
+          _searchText = _searchQuery.text;
+        });
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _IsSearching = false;
+    init();
+
+  }
+
+  void init() {
+    _list = ["The Catcher in the Rye", "One Flew Over the Cuckoo's nest", "Cat's Cradle",
+    "Catch-22", "JoJo's Bizzare Adventure", "Spark of Life", "A Moveable Feast"];
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+          centerTitle: true,
+          title: appBarTitle,
+          backgroundColor: Colors.deepOrangeAccent,
+          actions: <Widget>[
+            new IconButton(icon: actionIcon, onPressed: () {
+              setState(() {
+                if (this.actionIcon.icon == Icons.search) {
+                  this.actionIcon = new Icon(Icons.close, color: Colors.white,);
+                  this.appBarTitle = new TextField(
+                    controller: _searchQuery,
+                    style: new TextStyle(
+                      color: Colors.white,
+
+                    ),
+                    decoration: new InputDecoration(
+                        prefixIcon: new Icon(Icons.search, color: Colors.white),
+                        hintText: "Search...",
+                        hintStyle: new TextStyle(color: Colors.white)
+                    ),
+                  );
+                  _handleSearchStart();
+                }
+                else {
+                  _handleSearchEnd();
+                }
+              });
+            },),
+          ]
+      ),
+      key: key,
+      body: new ListView(
+        padding: new EdgeInsets.symmetric(vertical: 8.0),
+        children: _IsSearching ? _buildSearchList() : _buildList(),
+      ),
+    );
+  }
+
+  List<ChildItem> _buildList() {
+    return _list.map((contact) => new ChildItem(contact)).toList();
+  }
+
+  List<ChildItem> _buildSearchList() {
+    if (_searchText.isEmpty) {
+      return _list.map((contact) => new ChildItem(contact))
+          .toList();
+    }
+    else {
+
+      List<String> _searchList = [];
+      for (int i = 0; i < _list.length; i++) {
+        String  name = _list.elementAt(i);
+        if (name.toLowerCase().contains(_searchText.toLowerCase())) {
+          _searchList.add(name);
+        }
+      }
+      return _searchList.map((contact) => new ChildItem(contact))
+          .toList();
+    }
+  }
+
+
+  void _handleSearchStart() {
+    setState(() {
+      _IsSearching = true;
+    });
+  }
+
+  void _handleSearchEnd() {
+    setState(() {
+      this.actionIcon = new Icon(Icons.search, color: Colors.white,);
+      this.appBarTitle =
+      new Text("Search Sample", style: new TextStyle(color: Colors.white),);
+      _IsSearching = false;
+      _searchQuery.clear();
+    });
+  }
+
+}
+
+class ChildItem extends StatelessWidget {
+  final String name;
+  ChildItem(this.name);
+  @override
+  Widget build(BuildContext context) {
+    return new ListTile(title: new Text(this.name));
+  }
+
 }
